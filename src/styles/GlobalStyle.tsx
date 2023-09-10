4dfe48b22bae75e76a80a479dcedf1ff1a3d2e54
asaei623 <86418308+asaei623@users.noreply.github.com>
2023-09-11 04:51:11 +0900

    feat: GlobalStyle 설정

---
+++ src/styles/GlobalStyle.tsx
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from "styled-components";
+import PretendardBold from "../assets/fonts/Pretendard-Bold.woff2";
+import PretendardMedium from "../assets/fonts/Pretendard-Medium.woff2";
+import PretendardRegular from "../assets/fonts/Pretendard-Regular.woff2";
+
+export const GlobalStyle = createGlobalStyle`
+*{
+    margin:0;
+    padding:0;
+    box-sizing: border-box;
+
+    @font-face {
+        font-family: 'PretendardBold';
+        src: local('PretendardBold'), url(${PretendardBold}) format('woff');
+        font-style: normal;
+    }
+    @font-face {
+        font-family: 'PretendardMedium';
+        src: local('PretendardMedium'), url(${PretendardMedium}) format('woff');
+        font-style: normal;
+    }
+    @font-face {
+        font-family: 'PretendardRegular';
+        src: local('PretendardRegular'), url(${PretendardRegular}) format('woff');
+        font-style: normal;
+    }
+}`;
