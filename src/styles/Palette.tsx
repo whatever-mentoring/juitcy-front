d4e7588a7adc31e70d1fe80b40c09cc510265f9a
asaei623 <86418308+asaei623@users.noreply.github.com>
2023-09-12 01:53:56 +0900

    fix: 메인 컬러 코드 수정

--- src/styles/Palette.tsx
+++ src/styles/Palette.tsx
@@ -1,13 +1,13 @@
 export const Palette = {
     Grey: "#e7e7e7",
     Juni: {
-        main: "#ff886e",
+        main: "#FF8267",
         category: "#FFEFEC",
         ans1: "#FFAD9B",
         ans2: "#FF9984",
     },
     Cyni: {
-        main: "#4472ff",
+        main: "#3F6EFF",
         category: "#E7EDFF",
         ans1: "#9CB4FF",
         ans2: "#7294FF",
