8e3902df34c3714d971eb51cb5492ecb6b932faf
asaei623 <86418308+asaei623@users.noreply.github.com>
2023-09-12 16:54:35 +0900

    feat: category 생성

---
+++ src/components/Category.tsx
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+import { Palette } from "styles/Palette";
+import Typo from "styles/Typo";
+
+interface propsType {
+    id: number;
+    name: string;
+    selected: boolean;
+    onClick: (id: number) => void;
+}
+interface containerProps {
+    selected: boolean;
+}
+
+const Category = ({ id, name, selected, onClick }: propsType) => {
+    return (
+        <Container selected={selected} onClick={() => onClick(id)}>
+            <Typo.b3>{name}</Typo.b3>
+        </Container>
+    );
+};
+
+export default Category;
+
+const Container = styled.div<containerProps>`
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+
+    padding: 3px 10px;
+
+    border-radius: 30px;
+    cursor: pointer;
+
+    ${(props) =>
+        props.selected
+            ? `
+        border : 1.5px solid  ${Palette.Main};
+        background: ${Palette.Main15};
+        color : ${Palette.Main};
+    `
+            : `
+        border : 0.75px solid ${Palette.Black};
+        background: ${Palette.Gray0};
+        color : ${Palette.Black};
+    `};
+`;
