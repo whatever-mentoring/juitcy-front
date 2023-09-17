2851fa7fee012dbf04c4eabc485785ef74289044
asaei623 <86418308+asaei623@users.noreply.github.com>
2023-09-17 21:39:34 +0900

    fix: 디폴트로 전체 카테고리 선택

--- src/components/CategoryBar.tsx
+++ src/components/CategoryBar.tsx
@@ -6,7 +6,7 @@ import Typo from 'styles/Typo';
 
 const CategoryBar = () => {
   const [categories, setCategories] = useState([
-    { id: 1, name: '전체', selected: false },
+    { id: 1, name: '전체', selected: true },
     { id: 2, name: '일상', selected: false },
     { id: 3, name: '인간관계', selected: false },
     { id: 4, name: '사회생활', selected: false },
@@ -45,5 +45,5 @@ export default CategoryBar;
 
 const Container = styled(Column)`
   width: 100%;
-  gap: 7px;
+  gap: 6px;
 `;
