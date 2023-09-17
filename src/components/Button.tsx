e5c86e7127366cc9b1318894fc5b2eaa6996d91f
asaei623 <86418308+asaei623@users.noreply.github.com>
2023-09-18 02:58:46 +0900

    feat: CTABtn 스타일 추가

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import { Palette } from 'styles/Palette';
 import Typo from 'styles/Typo';
+import ctaArrow from 'assets/icons/cta-arrow.svg';
 
 export const LongBtn = ({ children }: { children: string }) => {
   return (
@@ -18,12 +19,29 @@ export const ShortBtn = ({ children }: { children: string }) => {
   );
 };
 
+export const CTABtn = ({ children }: { children: string }) => {
+  return (
+    <CTABtnContainer>
+      <Typo.b3 color={Palette.White}>{children}</Typo.b3>
+      <img src={ctaArrow}></img>
+    </CTABtnContainer>
+  );
+};
+
 const Container = styled.button<{ width?: number }>`
   width: ${({ width }) => (width ? `${width}px` : '100%')};
   height: 50px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
   border: none;
   border-radius: 10px;
   background: ${Palette.Main};
   cursor: pointer;
+`;
+const CTABtnContainer = styled(Container)`
+  width: 130px;
+  height: 33px;
 `;
