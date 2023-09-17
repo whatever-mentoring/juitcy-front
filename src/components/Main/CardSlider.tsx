1d1945365b86dd1dc1457a09a024deb026063f97
asaei623 <86418308+asaei623@users.noreply.github.com>
2023-09-17 14:16:45 +0900

    feat: CardSlider 생성

---
+++ src/components/Main/CardSlider.tsx
@@ -0,0 +1,65 @@
+import { Column, Row } from 'assets/common';
+import { AnsCard, QContentCard, QTitleCard } from 'components/Card';
+import { useState } from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import { styled } from 'styled-components';
+import { Palette } from 'styles/Palette';
+import PageIndicator, { Dot } from './PageIndicator';
+
+const CardSlider = () => {
+  const [currentSlide, setCurrentSlide] = useState<number>(0);
+  const settings = {
+    dots: false,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    swipeToSlide: true,
+    afterChange: (current: number) => {
+      setCurrentSlide(current);
+    },
+  };
+  const cards = [
+    <QTitleCard></QTitleCard>,
+    <QContentCard></QContentCard>,
+    <AnsCard></AnsCard>,
+    <AnsCard></AnsCard>,
+    <AnsCard></AnsCard>,
+  ];
+
+  return (
+    <Wrapper>
+      <StyledSlider {...settings}>
+        {cards.map((card, index) => card)}
+      </StyledSlider>
+      <PageIndicator>
+        {Array.from({ length: cards.length }, (_, index) => (
+          <Dot key={index} selected={index === currentSlide} />
+        ))}
+      </PageIndicator>
+    </Wrapper>
+  );
+};
+
+export default CardSlider;
+
+const Wrapper = styled(Column)`
+  height: 100%;
+`;
+const StyledSlider = styled(Slider)`
+  // .slick-slide div {
+  //   width: 100%;
+  // }
+  // .slider-wrapper {
+  //   padding: 20px;
+  // }
+  // .slick-dots li.slick-active button:before {
+  //   color: ${Palette.Main};
+  // }
+  // .slick-dots li button:before {
+  //   color: ${Palette.Gray2};
+  //   opacity: 1;
+  // }
+`;
