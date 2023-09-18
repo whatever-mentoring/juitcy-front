88d1e7a31a24e359146540fa968a29431728b5de
asaei623 <86418308+asaei623@users.noreply.github.com>
2023-09-19 08:17:42 +0900

    feat: 작성완료 버튼이 있는 하단바 생성

--- src/pages/Answer/WriteAnswer/index.tsx
+++ src/pages/Answer/WriteAnswer/index.tsx
@@ -2,6 +2,7 @@ import { Column, EntireContainer } from 'assets/common';
 import WriteInputBox from 'components/Answer/WriteInputBox';
 import { QDetailCard } from 'components/Card';
 import { Header } from 'components/Header';
+import WriteConfirmBar from 'components/WriteConfirmBar';
 import { Palette } from 'styles/Palette';
 
 const WriteAnswer = () => {
@@ -16,6 +17,7 @@ const WriteAnswer = () => {
           <WriteInputBox />
         </Column>
       </EntireContainer>
+      <WriteConfirmBar userType={2} />
     </Column>
   );
 };
