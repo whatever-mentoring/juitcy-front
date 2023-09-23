58ebae53e4a19b5f732cbb9ba3620216012d0215
asaei623 <86418308+asaei623@users.noreply.github.com>
2023-09-23 18:44:59 +0900

    fix: 소셜로그인시 userType 저장

--- src/network/apis/loginApi.ts
+++ src/network/apis/loginApi.ts
@@ -1,4 +1,5 @@
 import { publicInstance } from '../config';
+import closure from 'store/closure';
 
 export const loginApi = async ({
   code,
@@ -16,15 +17,11 @@ export const loginApi = async ({
     });
 
     //토큰 값을 localStorage에 저장
-    window.localStorage.setItem(
-      'access_token',
-      res.data.result.jwt.access_token,
-    );
-    window.localStorage.setItem(
-      'refresh_token',
-      res.data.result.jwt.refresh_token,
-    );
+    window.localStorage.setItem('access_token', res.data.result.access_token);
+    window.localStorage.setItem('refresh_token', res.data.result.refresh_token);
     //만료시간도 저장
+    //userType 저장
+    closure.setUserType(res.data.result.role);
 
     //회원가입/로그인 구분
     if (res.data.result.isNewUser) {
