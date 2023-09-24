b57d56778a9833c88c7e582f4a510986814cec7e
chaeyeonan <dkscodus1204@ewhain.net>
2023-09-24 19:03:04 +0900

    fix: loginApi oauth 로직 수정

--- src/network/apis/loginApi.ts
+++ src/network/apis/loginApi.ts
@@ -9,27 +9,14 @@ export const loginApi = async ({
   state: string;
 }) => {
   try {
-    let res = await publicInstance.get('/login/naver', {
+    const res = await publicInstance.get('/login/naver', {
       params: {
         code: code,
         state: state,
       },
     });
 
-    //토큰 값을 localStorage에 저장
-    window.localStorage.setItem('access_token', res.data.result.access_token);
-    window.localStorage.setItem('refresh_token', res.data.result.refresh_token);
-    //만료시간도 저장
-    //userType 저장
-    closure.setUserType(res.data.result.role);
-
-    //회원가입/로그인 구분
-    if (res.data.result.isNewUser) {
-      window.location.href = '/signup/terms';
-    }
-    {
-      window.location.href = '/';
-    }
+    return res;
   } catch (err) {
     console.log(err);
   }
