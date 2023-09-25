8b86a4e59cc74c517456a7a0a3413a57bf13eaf1
asaei623 <86418308+asaei623@users.noreply.github.com>
2023-09-25 21:15:26 +0900

    fix: 회원가입 버튼이 작아지는 문제 해결

--- src/pages/Signup/SignupSuccess/index.tsx
+++ src/pages/Signup/SignupSuccess/index.tsx
@@ -1,5 +1,6 @@
 import { Column, EntireContainer } from 'assets/common';
 import { LongBtn } from 'components/common/Button';
+import { Header } from 'components/common/Header';
 import styled from 'styled-components';
 import { Palette } from 'styles/Palette';
 import Typo from 'styles/Typo';
@@ -8,30 +9,32 @@ const SignupSuccess = () => {
   const userType = window.localStorage.getItem('userType');
 
   return (
-    <EntireContainer>
-      <InnerContainer>
-        <Column gap={11}>
-          <Typo.h1>회원가입 완료</Typo.h1>
-          <Typo.h2 color={Palette.Gray4}>
-            쥬잇씨 가입이 완료되었습니다. <br />
-          </Typo.h2>
-          <Typo.h2 color={Palette.Gray4}>
-            {userType === 'Juni'
-              ? '지금 바로 질문을 등록해보세요!'
-              : '주니어들의 고민을 확인하러 가볼까요?'}
-          </Typo.h2>
-        </Column>
-        <Img>
-          {userType === 'Juni' ? (
-            <img src="/img/pageImgs/signup-success-juni.svg" />
-          ) : (
-            <img src="/img/pageImgs/signup-success-cyni.svg" />
-          )}
-        </Img>
-
-        <LongBtn to="/">확인</LongBtn>
-      </InnerContainer>
-    </EntireContainer>
+    <Column>
+      <Header borderBottom={false}>{''}</Header>
+      <EntireContainer>
+        <InnerContainer>
+          <Column gap={11}>
+            <Typo.h1>회원가입 완료</Typo.h1>
+            <Typo.h2 color={Palette.Gray4}>
+              쥬잇씨 가입이 완료되었습니다. <br />
+            </Typo.h2>
+            <Typo.h2 color={Palette.Gray4}>
+              {userType === 'Juni'
+                ? '지금 바로 질문을 등록해보세요!'
+                : '주니어들의 고민을 확인하러 가볼까요?'}
+            </Typo.h2>
+          </Column>
+          <ImgWrapper>
+            {userType === 'Juni' ? (
+              <Img src="/img/pageImgs/signup-success-juni.svg" />
+            ) : (
+              <Img src="/img/pageImgs/signup-success-cyni.svg" />
+            )}
+          </ImgWrapper>
+          <LongBtn to="/">확인</LongBtn>
+        </InnerContainer>
+      </EntireContainer>
+    </Column>
   );
 };
 
@@ -41,11 +44,13 @@ const InnerContainer = styled(Column)`
   width: 100%;
   height: 100%;
   justify-content: space-between;
-
-  padding-top: 60px;
 `;
-const Img = styled(Column)`
+const ImgWrapper = styled(Column)`
   width: 100%;
+  height: 320px;
   justify-content: center;
   align-items: center;
+`;
+const Img = styled.img`
+  width: 250px;
 `;
