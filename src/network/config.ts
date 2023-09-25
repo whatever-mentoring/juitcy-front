a682753cfb0acf6b613ee79e9bbf7448411c03cf
asaei623 <86418308+asaei623@users.noreply.github.com>
2023-09-25 09:48:04 +0900

    fix: 로그인되지 않은 상태에서 페이지에 접근할 경우 로그인 화면으로 리다이렉트

--- src/network/config.ts
+++ src/network/config.ts
@@ -43,22 +43,29 @@ publicInstance.interceptors.response.use(
     const originalRequest = err.config;
     const { accessToken, refreshToken } = getTokens();
 
-    if (err.response && err.response.status === 401 && refreshToken) {
-      // access token이 만료되었고 refresh token이 있는 경우
-      try {
-        // 새로운 access token을 받아온 뒤 저장
-        const response = await publicInstance.post('/refreshToken', {
-          refreshToken,
-        });
-        const newAccessToken = response.data.accessToken;
-        setAccessToken(newAccessToken);
+    if (err.response && err.response.status === 401) {
+      if (refreshToken) {
+        // access token이 만료되었고 refresh token이 있는 경우
+        try {
+          // 새로운 access token을 받아온 뒤 저장
+          const response = await publicInstance.post('/refreshToken', {
+            refreshToken,
+          });
+          const newAccessToken = response.data.accessToken;
+          setAccessToken(newAccessToken);
 
-        // 이전 요청을 재시도
-        publicInstance.defaults.headers.common['Authorization'] =
-          newAccessToken;
-        return publicInstance(originalRequest);
-      } catch (err) {
-        // refresh token이 유효하지 않은 경우
+          // 이전 요청을 재시도
+          publicInstance.defaults.headers.common['Authorization'] =
+            newAccessToken;
+          return publicInstance(originalRequest);
+        } catch (err) {
+          // refresh token이 유효하지 않은 경우
+          // 로그아웃
+          localStorage.clear();
+          window.location.href = '/signup';
+        }
+      } else {
+        //access token, refresh token이 없는 경우 (로그인되지 않은 상태에서의 접근)
         // 로그아웃
         localStorage.clear();
         window.location.href = '/signup';
