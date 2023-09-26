09404981737ef93b359ac6ee3046c06ebbc61124
asaei623 <86418308+asaei623@users.noreply.github.com>
2023-09-26 19:03:22 +0900

    fix: 사파리를 위한 스타일 추가

--- src/components/common/Card.tsx
+++ src/components/common/Card.tsx
@@ -138,6 +138,7 @@ const Container = styled(Column)<{ height?: number; color: string }>`
 
   border-radius: 16px;
   border: 1.5px solid ${Palette.Main};
+  -webkit-border: 1.5px solid ${Palette.Main};
   background: ${({ color }) => color};
   box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
 `;
